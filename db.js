612858a7ddcd893c7496028cbd5744e9a19251f0
Rizwan.dev <rizwanofficial70@gmail.com>
2024-12-19 10:20:59 +0530

    shift mongoDB to online server

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const mongourl = 'mongodb://127.0.0.1:27017'
+require('dotenv').config();
+
+//const mongourl = process.env.DB_LOCAL_URL
+const mongourl = process.env.DB_URL
 
 mongoose.connect(mongourl, { 
     useNewUrlParser: true, 
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const db = require('./db')
+require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()) //req.body
 const personRoutes = require('./routes/PersonRoutes');
-const menuitemRoutes = require('./routes/manuItemRoute')
+const menuitemRoutes = require('./routes/manuItemRoute');
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req,res)=>{
     res.send("Server created");
@@ -14,7 +17,6 @@ app.get('/', (req,res)=>{
 app.use('/person', personRoutes);
 app.use('/menuitem',menuitemRoutes);
 
-
-app.listen(3000 , ()=>{
+app.listen(PORT, ()=>{
     console.log("Server is lesting PORT 3000");
 })
